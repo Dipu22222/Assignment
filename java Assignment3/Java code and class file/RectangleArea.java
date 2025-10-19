1fa0dd2190e7be817a31a77e48b88d727e42e285
Dipu22222 <dipumondol974@gmail.com>
2025-10-20 00:54:19 +0600

    Add files via upload

---
+++ java Assignment3/Java code and class file/RectangleArea.java
@@ -0,0 +1,17 @@
+public class RectangleArea {
+    public static void main(String[] args) {
+        if (args.length != 2) {
+            System.out.println("Please enter width and height as command line arguments.");
+            return;
+        }
+
+        double width = Double.parseDouble(args[0]);
+        double height = Double.parseDouble(args[1]);
+
+        double area = width * height;
+
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Area: " + area);
+    }
+}
